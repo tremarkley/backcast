793a99413bdf84e5d372c14c3db1fca99bc12c19
tremarkley <tremarkley@gmail.com>
2018-01-30 11:33:20 -0800

    Include api key in specrunner and index.html, setup ajax request for fetching data from YouTube API

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -1,5 +1,34 @@
 var Videos = Backbone.Collection.extend({
 
-  model: Video
-
+  model: Video,
+  
+  parse: function(dataObj) {
+    return dataObj.items;
+  },
+  
+  search: function(userString) {
+    Backbone.ajax({
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      type: 'GET',
+      data: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5, 
+        q: userString,
+        key: window.YOUTUBE_API_KEY 
+      },
+      contentType: 'application/json',
+      
+      success: function(data) {
+        console.log(data);
+        collection.parse(data);
+      },
+      
+      error: function(data) {
+        console.log('ERROR', data);
+      }
+       
+      
+    });
+  }
 });
